fb2e34ef2d3d4c22c546c0f3194d1dcb65181337
CamilaDuqueJimenez <aprendizds@ofima.com>
2025-07-04 14:30:07 -0500

    Add AppDbContext with DbSet properties for Empleado, Room, and User entities

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using EmpleadosApi.Models;
+using ApiEmpleados_Backend.Domain.Entities;
 
 namespace EmpleadosApi.Data
 {
@@ -8,5 +9,17 @@ namespace EmpleadosApi.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.CreatedRooms)
+                .WithOne(r => r.Creator)
+                .HasForeignKey(r => r.CreatorId);
+        }
     }
 }
